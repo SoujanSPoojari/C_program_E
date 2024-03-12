0dbcba1337db154d58b138e6a3ec9bea36827315
SoujanSPoojari <60816984+SoujanSPoojari@users.noreply.github.com>
2024-03-13 00:29:08 +0530

    C20_two_array.c

--- C20_two_array.c
+++ C20_two_array.c
@@ -1,28 +1,43 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,n,m;
-    int arr[n];
-    int count;
+    int i,j,n,m,k;
+    int arr1[50],arr2[50];
+    int count=0;
     printf("Enter the array1 size : ");
     scanf("%d",&n);
     printf("Enter the array2 size : ");
     scanf("%d",&m);
     printf("Enter the array1 elements : ");
-    while(i!=n)
+    // while(i!=n)
+    // {
+    // scanf("%d",&i);
+    for(i=0;i<n;i++)
     {
-    scanf("%d",&i);
+        scanf("%d",&arr1[i]);
+    }
     printf("Enter the array2 elements : ");
-    while(j!=m)
+    for(i=0;i<m;i++)
     {
-    scanf("%d",&j);
-        j++;
+        scanf("%d",&arr2[i]);
     }
-        i++;
-    }
-    if(arr[i]==arr[j])
+    if(n!=m)
     {
-        count++;
+        printf("Array elements are not equal.");
+        return 0;
+    }
+    for(j=0;j<n;j++)
+    {
+        for(k=0;k<m;k++)
+        {
+            if(arr1[j]==arr2[k])
+            {
+                count++;
+            }
+        }
+    }
+    if(count==m)
+    {
         printf("Array elements are equal.");
     }
     else
